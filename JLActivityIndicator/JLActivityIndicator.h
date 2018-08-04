1e11e661300a27e21c72962d32fa0e4b355b225e
Jing Wei Li <ljw9801055@gmail.com>
2018-08-04 15:17:35 -0400

    Initial Commit

---
+++ JLActivityIndicator/JLActivityIndicator.h
@@ -0,0 +1,19 @@
+//
+//  JLActivityIndicator.h
+//  JLActivityIndicator
+//
+//  Created by Jing Wei Li on 8/4/18.
+//  Copyright © 2018 Jing Wei Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JLActivityIndicator.
+FOUNDATION_EXPORT double JLActivityIndicatorVersionNumber;
+
+//! Project version string for JLActivityIndicator.
+FOUNDATION_EXPORT const unsigned char JLActivityIndicatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JLActivityIndicator/PublicHeader.h>
+
+
